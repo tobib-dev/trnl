e5462aafc61c5521be6749381fa465699253fae4
Tobi Balogun <tobibags19@gmail.com>
2026-02-08 12:46:45 -0600

    Test: verify write content

--- response_test.go
+++ response_test.go
@@ -3,6 +3,7 @@ package trnl
 import (
 	"bufio"
 	"bytes"
+	"strings"
 	"testing"
 )
 
@@ -32,11 +33,55 @@ func TestWriteHeader(t *testing.T) {
 				defer func() {
 					if r := recover(); r == nil {
 						t.Errorf("expected panic for status code: %d, but did not panic", tt.have)
+						return
 					}
 				}()
 			}
 
 			res.WriteHeader(tt.have)
+		})
+	}
+}
+
+func TestWriteResponse(t *testing.T) {
+	tests := []struct {
+		name string
+		body []byte
+		want string
+	}{
+		{"valid content", []byte("Hello, world!"), "HTTP/1.1 200 OK\r\n\r\nHello, world!"},
+		{"empty content", []byte{}, "HTTP/1.1 200 OK\r\n\r\n"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			res := response{
+				writer:      bufio.NewWriter(&buf),
+				header:      Header{},
+				protVer:     "HTTP/1.1",
+				wroteHeader: false,
+			}
+
+			n, err := res.Write(tt.body)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+				return
+			}
+
+			res.writer.Flush()
+
+			msg := strings.Split(tt.want, "\r\n\r\n")
+
+			if n != len(strings.TrimSpace(msg[1])) {
+				t.Errorf("got - %d bytes to be written, want %d", n, len(tt.want))
+				return
+			}
+
+			if buf.String() != tt.want {
+				t.Errorf("got - %s, want %s", buf.String(), tt.want)
+				return
+			}
 		})
 	}
 }
