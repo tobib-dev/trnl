de300f9324a95bd59ea5efd39376f48846711164
Tobi Balogun <tobibags19@gmail.com>
2026-02-22 10:47:48 -0600

    Refactor: Implement match routes for trie router and rhttp methods, add readme

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # TRNL 
 
-Trnl is a lightweight HTTP Package written in Go
+Trnl is a lightweight HTTP package written in Go.
+
+## Motivation
+
+This package was an opportunity to get into the TCP transport layer and experiment with the socket API provided by the Go net package. This app was also heavily inspired by the Go standard library net/http module, hence, the similarity between both modules.
+
+Note: This is not meant to be a replacement for the net/http package or frameworks like Gin-Gonic or other production-ready libraries. In many ways, it was a way to see how far I can go with vanilla net package. Please feel free to test, extend, and use the module however you like.
+
+## Installation
+
+Trnl can be installed by running the command:
+```
+go get github.com/tobib-dev/trnl
+```
+
+Note: Installation of Go on your local machine is necessary to install this package
+
+## Usage
+
+Trnl can be used like standard HTTP packages are used. Here are some of the most used functions
+
+### Generate Default Multiplexer
+```
+mux := trnl.Default()
+```
+
+### Initialize Server
+```
+srv = &trnl.Server{
+  Addr: ":{PORT_NUMBER}",
+  Handler: mux,
+  ReadTimeout: 500 // Time Duration in milliseconds
+}
+```
--- tree.go
+++ tree.go
@@ -6,23 +6,26 @@ import (
 	"strings"
 )
 
-var validRoutes = []string{"GET", "POST", "UPDATE", "DELETE"}
+var validRoutes = []string{"GET", "POST", "PUT", "DELETE"}
 
-type RadixNode struct {
-	children       map[string]*RadixNode // Map parent node to children node
-	handlers       map[string]Handler    // Map HTTP methods to handler functions
-	isDynamicRoute bool
-	param          string
-	endOfPath      bool
+type rNode struct {
+	children     map[string]*rNode  // Map subpath string to children node
+	handlers     map[string]Handler // Map HTTP methods to handler functions
+	dynamicRoute bool
+	param        string
+	endOfPath    bool
 }
 
 type TrieRouter struct {
-	root *RadixNode
+	root *rNode
 }
 
+/*
+ * Registers user defined endpoint to the router trie
+ */
 func (t *TrieRouter) addRoute(method, path string, handlr any) {
 	if !slices.Contains(validRoutes, method) {
-		panic("method must be one of the following: GET, POST, UPDATE, DELETE")
+		panic("method must be one of the following: GET, POST, PUT, DELETE")
 	}
 
 	if path == "" || path[0] != '/' {
@@ -35,21 +38,21 @@ func (t *TrieRouter) addRoute(method, path string, handlr any) {
 	for _, seg := range segments {
 		if strings.HasPrefix(seg, ":") {
 			// Handle dynamic segment
-			paramName := seg[1:]
+			prm := seg[1:]
 
-			// Create child node and set to dynamic
+			// Create child node if it doesn't exist and set to dynamic
 			child, ok := node.children[":"]
 			if !ok {
-				child = &RadixNode{
-					isDynamicRoute: true,
-					param:          paramName,
-					children:       make(map[string]*RadixNode),
-					handlers:       make(map[string]Handler),
+				child = &rNode{
+					dynamicRoute: true,
+					param:        prm,
+					children:     make(map[string]*rNode),
+					handlers:     make(map[string]Handler),
 				}
 
 				node.children[":"] = child
-			} else if child.param != paramName && child.param != "" {
-				msg := fmt.Sprintf("conflicting param names for dynamic segments: %s => %s - %s", seg, child.param, paramName)
+			} else if child.param != prm && child.param != "" {
+				msg := fmt.Sprintf("conflicting param names for dynamic segments: %s => %s - %s", seg, child.param, prm)
 				panic(msg)
 			}
 			node = child
@@ -57,14 +60,16 @@ func (t *TrieRouter) addRoute(method, path string, handlr any) {
 			// Handle static segment
 			_, ok := node.children[seg]
 			if !ok {
-				node.children[seg] = &RadixNode{
-					children: make(map[string]*RadixNode),
+				node.children[seg] = &rNode{
+					children: make(map[string]*rNode),
 					handlers: make(map[string]Handler),
 				}
 			}
 			node = node.children[seg]
 		}
 	}
+
+	// Check if method exist on path after adding subpaths
 	if _, exists := node.handlers[method]; exists {
 		panic("already registered route: " + method + " " + path)
 	}
@@ -81,22 +86,65 @@ func (t *TrieRouter) addRoute(method, path string, handlr any) {
 	node.endOfPath = true // Mark that at least one handler exists at this path
 }
 
-func filterEmpty(parts []string) []string {
-	filteredPath := parts[:0]
+/*
+ * Returns handler, parameters, and boolean value verifying if route is static or dynamic.
+ * Will return nil handler if method is not allowed or no static nor dynamic routes are
+ * found
+ */
+func (t *TrieRouter) matchRoute(method, path string) (Handler, map[string]string, bool) {
+	node := t.root
+	segments := filterEmpty(strings.Split(path, "/"))
+	params := make(map[string]string)
+	hasParams := false
 
+	for _, seg := range segments {
+		// Direct match on a static route
+		if child, ok := node.children[seg]; ok {
+			node = child
+			continue
+		}
+
+		// Dynamic routes
+		if child, ok := node.children[":"]; ok {
+			node = child
+			if node.param != "" {
+				params[node.param] = seg
+				hasParams = true
+			}
+			continue
+		}
+
+		return nil, params, hasParams // return nil handler if no static or dynamic routes
+	}
+
+	// Check if method is allowed for path
+	handlr, ok := node.handlers[method]
+	if !ok {
+		return nil, params, hasParams // method not allowed
+	}
+	return handlr, params, hasParams // return handler, params if found and boolean (true - has params, false - no params)
+}
+
+func filterEmpty(parts []string) []string {
+	fPath := parts[:0]
+
+	// users//:id ["users", "", ":id"] => ["users", ":id"]
 	for _, p := range parts {
 		if p != "" {
-			filteredPath = append(filteredPath, p)
+			fPath = append(fPath, p)
 		}
 	}
 
-	return filteredPath
+	return fPath
 }
 
+/*
+ * Initializes router trie and return router address to user
+ */
 func DefaultTrieRouter() *TrieRouter {
 	return &TrieRouter{
-		root: &RadixNode{
-			children: make(map[string]*RadixNode),
+		root: &rNode{
+			children: make(map[string]*rNode),
 			handlers: make(map[string]Handler),
 		},
 	}
@@ -104,4 +152,16 @@ func DefaultTrieRouter() *TrieRouter {
 
 func (r *TrieRouter) Get(path string, handler any) {
 	r.addRoute("GET", path, handler)
+}
+
+func (r *TrieRouter) Post(path string, handler any) {
+	r.addRoute("POST", path, handler)
+}
+
+func (r *TrieRouter) Put(path string, handler any) {
+	r.addRoute("PUT", path, handler)
+}
+
+func (r *TrieRouter) Delete(path string, handler any) {
+	r.addRoute("DELETE", path, handler)
 }
